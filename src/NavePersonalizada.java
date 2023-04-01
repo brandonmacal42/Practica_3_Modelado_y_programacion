ef4920b3694d90f5c4884f11af6393d49b9ddeee
Moises2136 <110215515+Moises2136@users.noreply.github.com>
2023-04-01 12:13:23 -0600

    Update NavePersonalizada.java

--- src/NavePersonalizada.java
+++ src/NavePersonalizada.java
@@ -14,20 +14,47 @@ public class NavePersonalizada extends ArmadoBuilder{
     
 
     public void agregarArma(){
+
+        if(this.ArmaU == 1)
+        componentes.add(new ArmaLaserSimple());
+        else if(this.ArmaU == 2)
         componentes.add(new ArmaMisilPlasma());
+        else if(this.ArmaU == 3)
+        componentes.add(new ArmaLaserDestructor());
+
     }
 
     public void agregarCabina(){
-        componentes.add(new CabinaPequena());
 
+        if(this.CabinaU == 1)
+        componentes.add(new CabinaUnPiloto());
+        else if (this.CabinaU == 2)
+        componentes.add(new CabinaPequena());
+        else if (this.CabinaU == 3)
+        componentes.add(new CabinaEjercito());
+        
     }
 
     public void agregarBlindaje(){
+
+        if(this.BlindajeU == 1)
+        componentes.add(new BlindajeSimple());
+        else if (this.BlindajeU == 2)
         componentes.add(new BlindajeReforzado());
+        else if (this.BlindajeU == 3)
+        componentes.add(new BlindajeFortaleza());
+        
     }
 
     public void agregarSistemaPropulsion(){
+
+        if(this.SistemaU == 1)
+        componentes.add(new PropulsionIntercontinental());
+        else if(this.SistemaU == 2)
         componentes.add(new PropulsionInterplanetaria());
+        else if(this.SistemaU == 3)
+        componentes.add(new PropulsionIntergalactica());
+        
     }
 
 }
