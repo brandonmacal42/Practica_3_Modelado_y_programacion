bfa2a9ecd72b0b7c2c59e4ee39f24c0acc7a0f1f
Moises2136 <110215515+Moises2136@users.noreply.github.com>
2023-04-01 17:18:06 -0600

    Update ArmadoBuilder.java

--- src/ArmadoBuilder.java
+++ src/ArmadoBuilder.java
@@ -1,3 +1,12 @@
+/**
+ * Clase ArmadoBuilder
+ * Contiene el patron de disenio Builder
+ * @author Moisés Corpus García
+ * @author Etni Sarai Castro Sierra
+ * @author Brandon Brayan Macal Cruz
+ * @version 1.0 Marzo 2023
+ */
+
 import java.util.LinkedList;
 
 import javax.sound.midi.VoiceStatus;
@@ -5,6 +14,11 @@ import javax.sound.midi.VoiceStatus;
 public abstract class ArmadoBuilder{
     protected LinkedList<Componentes> componentes = new LinkedList<Componentes>();
 
+    /**
+    * Metodo que regresa el costo total de una nave
+    * @param costotal cantidad de dinero a pagar de la nave
+    * @return costotal Cantidad de dinero a pagar de la nave
+    */
     public double CostoFinal(){
         float costototal = 0;
 
@@ -14,6 +28,9 @@ public abstract class ArmadoBuilder{
         return costototal;
     }
 
+    /**
+    * Metodo que imprime las piezas que lleva la nave
+    */
     public void getArmado(){
    
         for (Componentes com : componentes) {
@@ -22,6 +39,11 @@ public abstract class ArmadoBuilder{
         }		
      }
 
+    /**
+    * Metodo que regresa el peso total de una nave
+    * @param pesototal peso de la nave
+    * @return pesototal peso final de la nave
+    */
      public int getPesoT(){
         int pesototal = 0;
         for (Componentes com : componentes) {
@@ -30,6 +52,11 @@ public abstract class ArmadoBuilder{
         return pesototal;
      }
     
+    /**
+    * Metodo que regresa el ataque total de una nave
+    * @param ataquetotal ataque de la nave
+    * @return ataquetotal ataque final de la nave
+    */
     public int getAtaqueT(){
         int ataquetotal = 0;
         for (Componentes com : componentes) {
@@ -38,6 +65,11 @@ public abstract class ArmadoBuilder{
         return ataquetotal;
      }
 
+    /**
+    * Metodo que regresa la Defensa total de una nave
+    * @param defensatotal defensa de la nave
+    * @return defensatotal defensa final de la nave
+    */
      public int getDefensaT(){
         int defensatotal = 0;
         for (Componentes com : componentes) {
@@ -46,6 +78,11 @@ public abstract class ArmadoBuilder{
         return defensatotal;
      }
 
+    /**
+    * Metodo que regresa la velocidad total de una nave
+    * @param velocidadtotal velocidad de la nave
+    * @return velocidadtotal velocidad final de la nave
+    */
      public int getVelocidadT(){
         int velocidadtotal = 0;
         for (Componentes com : componentes) {
